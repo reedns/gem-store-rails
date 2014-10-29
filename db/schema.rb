4ca9de1b9b62c011e0feb0cd288520c09a0882b9
Reed Nelson-Saunders <reedns@hotmail.com>
2014-10-28 21:18:24 -0700

    add relationship between category and product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029013850) do
+ActiveRecord::Schema.define(version: 20141029040429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 20141029013850) do
     t.integer  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
 end
