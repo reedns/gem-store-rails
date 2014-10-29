3133b94575cb1c3a08609b1ed7167f68f38a2d32
Reed Nelson-Saunders <reedns@hotmail.com>
2014-10-28 18:19:53 -0700

    extract json helper method

--- test/integration/listing_products_test.rb
+++ test/integration/listing_products_test.rb
@@ -10,15 +10,15 @@ class ListingProductsTest < ActionDispatch::IntegrationTest
 
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
-    assert_equal Product.count, JSON.parse(response.body).size
+    assert_equal Product.count, json(response.body).size
   end
 
-  test 'lists lowest priced products' do
+  test 'lists books by price products' do
     get '/products?price=110.50'
 
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
-    assert_equal 1, JSON.parse(response.body).size
+    assert_equal 1, json(response.body).size
 
   end
 end
