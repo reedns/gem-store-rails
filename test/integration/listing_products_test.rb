6e45435f70edd45b08703de92223a792b9d01a63
Reed Nelson-Saunders <reedns@hotmail.com>
2014-10-28 18:16:15 -0700

    list books with price filter

--- test/integration/listing_products_test.rb
+++ test/integration/listing_products_test.rb
@@ -12,4 +12,13 @@ class ListingProductsTest < ActionDispatch::IntegrationTest
     assert_equal Mime::JSON, response.content_type
     assert_equal Product.count, JSON.parse(response.body).size
   end
+
+  test 'lists lowest priced products' do
+    get '/products?price=110.50'
+
+    assert_equal 200, response.status
+    assert_equal Mime::JSON, response.content_type
+    assert_equal 1, JSON.parse(response.body).size
+
+  end
 end
