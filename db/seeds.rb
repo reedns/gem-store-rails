92118889faeba8ceeed68b093bf4ac0e4b4a0641
Reed Nelson-Saunders <reedns@hotmail.com>
2014-10-29 20:28:18 -0700

    add more attributes to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,32 @@
-Product.create!(name: 'Bircon', description: 'It is cool', price: 23)
+Product.create!(name: "Azurite",
+                description: "Some gems have hidden qualities beyond their
+                              luster, beyond their shine... Azurite is one of
+                              those gems.",
+                shine: 8,
+                price: 110.50,
+                rarity: 7,
+                color: "#CCC",
+                faces: 14,
+                image: "images/gem-02.gif")
+
+Product.create!(name: "Bloodstone",
+                description: "Origin of the Bloodstone is unknown, hence its
+                              low value. It has a very high shine and 12 sides
+                              , however.",
+                shine: 9,
+                price: 22.90,
+                rarity: 6,
+                color: "#CCC",
+                faces: 12,
+                image: "images/gem-02.gif")
+
+Product.create!(name: "Zircon",
+                description: "Zircon is our most coveted and sought after gem.
+                              You will pay much to be the proud owner of this
+                              gorgeous and high shine gem.",
+                shine: 70,
+                price: 1100,
+                rarity: 2,
+                color: "#CCC",
+                faces: 6,
+                image: "images/gem-02.gif")
